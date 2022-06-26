b934f0521f0cb9dc537aaef9c42c5e5b3b35393a
Himanshu Seth <88048010+SethHimanshu1708@users.noreply.github.com>
2022-06-26 08:56:10 +0530

    Add files via upload

---
+++ src/component/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an about component!
+
+        <p className='my-5'>This is a React project. I have used react, bootstrap and react router.
+        You can add or delete todos in this project according to your wish.
+        
+        !! Thank you for checking this project !!</p>
+    </div>
+  )
+}
---
+++ src/component/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  
+  return (
+    <div className='bg-dark text-light py-2 my-3' >
+      <p className="text-center">
+        Copyright &copy; MyTodoList.com
+      </p>
+    </div>
+  )
+}
